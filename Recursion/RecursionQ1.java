8b6d4a597af4e15d893582fe5689025334136c0a
ZenithDev1 <devendra702321@gmail.com>
2025-01-17 23:38:00 +0530

    Update RecursionQ1.java

--- Recursion/RecursionQ1.java
+++ Recursion/RecursionQ1.java
@@ -1,4 +1,5 @@
-public class RecursionQ1 {   
+public class RecursionQ1 { 
+    // this is our main() metho;
     public static void main(String[] args) {
       int result = sum(7);
       System.out.println(result);
