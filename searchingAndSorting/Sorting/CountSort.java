1e3e058e37dff8de789dfbfc3488a8e44ea70ae1
ZenithDev1 <devendra702321@gmail.com>
2025-05-18 18:04:42 +0530

    Update CountSort.java

--- searchingAndSorting/Sorting/CountSort.java
+++ searchingAndSorting/Sorting/CountSort.java
@@ -1,37 +1,37 @@
 package DSA;
+import java.util.*;
 
-public class CountSort {
+class CountSort{
     public static void main(String[] args) {
-        int n =6;
+        
         int[] arr = {6,2,6,6,2,4};
+        int n =arr.length-1;
+        
         int max = 0;
-          // finding max
-        for(int i = 0; i<= arr.length-1; i++){
+        for(int i =0; i<=n; i++){
             if(max < arr[i]){
                 max = arr[i];
             }
-        }System.out.println("Largest number of the array: "+max);
-
-
-        int[] Count = new int[max +1 ];
-       
-        for (int i = 0; i<n; i++){
+        }
+        System.out.println("Maximum number in arr: "+max);
+        
+        int [] Count = new int[max + 1];
+        
+        for(int i = 0; i<=n; i++){
             int element = arr[i];
-            Count[element]++;
+            Count[element]++; // Increase count of this element from arr[i]
         }
-
-        int k=0;
-        for(int i=max; i>=0; i--){
-            int freq = Count[i];
-            for(int j =1; j<=freq; j++){
-                arr[k] = i;
-                k++;
-            }
-        }
-
-        System.out.println("pronting the sorted array in decreasing order");
-        for(int i=0; i<n; i++){
-            System.out.print(arr[i]+" ");
-        }
+        System.out.println(Arrays.toString(Count));
+        
+        
+        int k = 0;
+        for(int i = Count.length-1; i>=0; i--){
+          int frequency = Count[i];
+          for(int j = 1; j <=frequency; j++){
+            arr[k]=i;
+            k++;
+          }  // j's loop till here
+        }   // i's loop is till here
+        System.out.println(Arrays.toString(arr)); // you can use for loop also
     }
 }
