58392b606526af9e65daaf6555d395e451b168c4
ZenithDev1 <devendra702321@gmail.com>
2025-07-19 23:51:43 +0530

    Create ExceptionEx.java

---
+++ Exception Handlind/ExceptionEx.java
@@ -0,0 +1,17 @@
+public class ExceptionEx {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+
+        try {
+            int result = a / b;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Cannot divide by zero!");
+        } finally {
+            System.out.println("This block always runs.");
+        }
+
+        System.out.println("Program continues...");
+    }
+}
